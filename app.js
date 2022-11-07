b8acbcaec4f9c6c5e5129891ebbee234d3f1cb5d
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-11-07 18:54:54 +0100

    Feat: Error handle for unhandled routes

--- app.js
+++ app.js
@@ -30,4 +30,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//Handling unhandled Routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find this ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
