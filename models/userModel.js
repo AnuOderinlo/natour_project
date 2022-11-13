8b5d0509e76294c5f7cc45418cf301541c5861e6
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-11-13 15:57:33 +0100

    Feat: Implemented signup API

---
+++ models/userModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const slugify = require('slugify');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Your name is requiured'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  phone: {
+    type: String,
+    unique: true,
+    // required: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'A password is required'],
+    maxlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // THIS ONLY WORKS ON SAVE OR CREATE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // this delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+//create a model from the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
