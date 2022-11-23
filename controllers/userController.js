415bc12a3cab253c074bdd7b4278e66adbcd9a27
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-11-23 22:41:44 +0100

    feat: Implemeted the update user data API and delete User API

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,7 +66,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 exports.deleteMe = catchAsync(async (req, res, next) => {
   const user = await User.findByIdAndUpdate(req.user.id, { active: false });
 
-  res.status(200).json({
+  res.status(204).json({
     status: 'success',
     data: null,
   });
