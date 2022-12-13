a65a5e18b59a8431b9dd4fc9c2ef4163806382ac
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-12-13 04:53:10 +0100

    feat: Implemented helmet, a security middleware to set some HTTP hearders

--- app.js
+++ app.js
@@ -7,20 +7,27 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
 const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+
+const app = express();
+//Development logging
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Set security HTTP headers
+app.use(helmet());
+
+// Limit request from same API
 const limiter = rateLimiter({
   windowMs: 60 * 60 * 1000, // 60mins or 1 hour
   max: 100,
   message: 'Too many request from this IP, please try again in an hour',
 });
 
-const app = express();
-
-app.use(express.json());
-
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
