a908ddff0afa65f0486a68acb264f010dfe211ce
Anuoluwpao <oderinloanuoluwapo@gmail.com>
2023-01-12 14:03:41 +0100

    feat: Implemeted the API to get all tours within a location

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -102,3 +102,34 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+// '/tours-within/:distance/center/:latlng/unit/:unit'
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        `Please provide latitude and longitude in this format lat,lng`,
+        400
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  console.log(distance, unit, lat, lng, radius);
+
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      data: tours,
+    },
+  });
+});
